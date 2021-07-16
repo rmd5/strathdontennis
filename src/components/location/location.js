29af1b7dc761a477a4a00a1151f01ec29ecbd60f
rmd5 <rmd5@hw.ac.uk>
2021-07-16 03:22:47 +0100

    Add google map

--- src/components/location/location.js
+++ src/components/location/location.js
@@ -6,7 +6,13 @@ function Location(props) {
     return (
         <div>
             <Heading />
-            <Footer />            
+            <div style={{ textAlign: "center", margin: "15px 0px" }}>
+                <div style={{ width: "546px", height: "450px", backgroundColor: "#ffffff80", maxWidth: "calc(100% - 30px)", display: "inline-block" }}>
+                    {/* <div style={{zIndex: "-1", position: "absolute", top: "50%", height: "50px", width: "50px", border: "1px solid black"}}></div> */}
+                    <iframe width={"100%"} height="450" style={{ border: "0", zIndex: "2" }} loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?q=57%C2%B012'13.3%22N%203%C2%B004'14.7%22W&key=AIzaSyANl228X4VdoJ0WImN_k7AHs-Ovfki1M7w"></iframe>
+                </div>
+            </div>
+            <Footer />
         </div>
     );
 }
