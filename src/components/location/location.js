e6979c1024119200c9dd965db173bdc6bc5a4846
rmd5 <rmd5@hw.ac.uk>
2021-07-16 10:02:02 +0100

    Add google maps

--- src/components/location/location.js
+++ src/components/location/location.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Footer from '../heading/footer';
 import Heading from '../heading/heading';
+import Iframe from './iframe';
 
 function Location(props) {
     return (
         <div>
             <Heading />
+            <div style={{textAlign: "center"}}>
+                <Iframe />
+            </div>
             <Footer />            
         </div>
     );
